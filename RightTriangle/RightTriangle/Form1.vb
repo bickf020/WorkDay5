1e8f46aac26d8df791f95acdfea97456e4153a12
Alexander Bickford <bickf020@umn.edu>
2013-10-08 12:05:04 -0500

    Finally got it to push

--- RightTriangle/RightTriangle/Form1.vb
+++ RightTriangle/RightTriangle/Form1.vb
@@ -4,8 +4,24 @@ Public Class Form1
 
     Private Sub btnCalculate_Click(sender As System.Object, e As System.EventArgs) Handles btnCalculate.Click
 
-        txtHyp.Text = "Not Implemented Yet"
+        Dim X As Double = CDbl(txtSideOne.Text)
+        Dim Y As Double = CDbl(txtSideTwo.Text)
+
+        Dim z As Double = Hypotenuse(X, Y)
+
+        txtHyp.Text = CStr(z)
 
     End Sub
+
+    Function Hypotenuse(ByVal A As Double, ByVal B As Double) As Double
+
+        Dim c As Double
+
+        c = Math.Sqrt(A ^ 2 + B ^ 2)
+
+        Return c
+
+    End Function
+
 
 End Class
